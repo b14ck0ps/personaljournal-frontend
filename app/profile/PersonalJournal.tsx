a16197fec33f2f69d1bfa81af412a9ca0c613388
Azran Hossain <mr69751@gmail.com>
2023-04-25 23:17:27 +0600

    show personal journal in profile page

---
+++ app/profile/PersonalJournal.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
+import axiosWithAuth from '../lib/AxiosAuth';
+import { JournalEntry } from '../Types';
+
+export default function PersonalJournalEntries() {
+    const router = useRouter();
+
+    const [entries, setEntries] = useState<JournalEntry[]>([]);
+
+    useEffect(() => {
+        axiosWithAuth.get(`/journalEntry/username/${sessionStorage.getItem('username')}`)
+            .then(response => {
+                if (response.status === 200) {
+                    const formattedEntries = response.data.map((entry: JournalEntry) => ({
+                        ...entry,
+                        createdAt: new Date(...entry.createdAt),
+                        updatedAt: new Date(...entry.updatedAt)
+                    }));
+                    setEntries(formattedEntries);
+                } else {
+                    router.push('/login'); // Redirect to login page
+                }
+            })
+            .catch(error => {
+                console.error(error);
+                router.push('/login'); // Redirect to login page on error
+            });
+    }, []);
+
+    return (
+        <div className="p-4">
+            <div className='flex justify-between mx-2'>
+                <h1 className="mb-4 text-xl font-bold">Personal Journal Entries</h1>
+                <a
+                    href='/journal/add'
+                    className='px-3 m-2 border border-amber-400 hover:bg-amber-400 '
+                >Add new</a>
+            </div>
+            <ul>
+                {entries.map(entry => (
+                    <li key={entry.id} className="p-4 mb-2 bg-gray-100 rounded-lg">
+                        <h2 className="text-lg font-bold">{entry.title}</h2>
+                        <p className="mb-2 text-sm text-gray-500">Created on {new Date(entry.createdAt).toLocaleDateString()}</p>
+                        <p className="text-gray-700">{entry.body}</p>
+                        <p className="mt-2 text-sm text-right text-gray-500">By {entry.user.name}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import axiosWithAuth from "../lib/AxiosAuth";
 import { UserProfile } from "../Types";
+import PersonalJournalEntries from "./PersonalJournal";
 
 export default function ProfilePage() {
     const [userProfile, setUserProfile] = useState<UserProfile | null>(null);
@@ -28,16 +29,19 @@ export default function ProfilePage() {
     }
 
     return (
-        <div className="flex flex-col items-center mt-5">
-            <img src={userProfile.image} alt="Profile" className="object-cover w-24 h-24 rounded-full" />
-            <h1 className="text-2xl font-bold">{userProfile.name}</h1>
-            <p className="text-gray-500">{userProfile.address}</p>
-            <p className="text-gray-500">{userProfile.email}</p>
-
-            <div className="flex gap-5 mt-5">
-                <a href="/profile/edit" className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">Edit</a>
+        <div>
+            <div className="flex flex-col items-center mt-5">
+                <img src={userProfile.image} alt="Profile" className="object-cover w-24 h-24 rounded-full" />
+                <h1 className="text-2xl font-bold">{userProfile.name}</h1>
+                <p className="text-gray-500">{userProfile.address}</p>
+                <p className="text-gray-500">{userProfile.email}</p>
+                <div className="flex gap-5 mt-5">
+                    <a href="/profile/edit" className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">Edit</a>
+                </div>
             </div>
-
+            <div className="w-3/4 m-auto">
+                <PersonalJournalEntries />
+            </div>
         </div>
     );
 }
