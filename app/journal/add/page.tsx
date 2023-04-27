96929b3454b03adfc946206b1acf91e4f456c516
Azran Hossain <mr69751@gmail.com>
2023-04-28 01:08:19 +0600

    insert tag when creating journal entity

--- app/journal/add/page.tsx
+++ app/journal/add/page.tsx
@@ -2,14 +2,21 @@
 import { useEffect, useState } from "react";
 import axiosWithAuth from "@/app/lib/AxiosAuth";
 
+interface Tag {
+    id: number;
+    name: string;
+}
+
 const JournalEntryField = () => {
     const [userId, setUserId] = useState<number | null>(null);
+    const [tags, setTags] = useState<Tag[]>([]);
+    const [selectedTag, setSelectedTag] = useState<Tag | null>(null);
+    const username = sessionStorage.getItem("username");
 
     const [title, setTitle] = useState("");
     const [body, setBody] = useState("");
 
     useEffect(() => {
-        const username = sessionStorage.getItem("username");
         axiosWithAuth
             .get(`/user/${username}`)
             .then((response) => {
@@ -18,21 +25,29 @@ const JournalEntryField = () => {
             .catch((error) => {
                 console.error(error);
             });
+
+        axiosWithAuth
+            .get("/tags")
+            .then((response) => {
+                setTags(response.data);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }, []);
-
-
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
         axiosWithAuth.post("/journalEntry", {
+            username: username,
             title: title,
             body: body,
-            user: { id: userId }
+            tagId: selectedTag?.id ?? 1,
         });
 
         window.location.href = "/";
-    };
+    }
 
     return (
         <form className="flex flex-col w-1/2 mx-auto mt-5"
@@ -46,6 +61,24 @@ const JournalEntryField = () => {
                 onChange={(e) => setTitle(e.target.value)}
                 className="p-1 my-2 border rounded-md"
             />
+
+            <label htmlFor="tag">Tag:</label>
+            <select
+                id="tag"
+                name="tag"
+                value={selectedTag?.id ?? ""}
+                onChange={(e) => {
+                    const tagId = parseInt(e.target.value);
+                    const tag = tags.find((t) => t.id === tagId) ?? null;
+                    setSelectedTag(tag);
+                }}
+                className="p-1 my-2 border rounded-md"
+            >
+                <option value="">Select a tag</option>
+                {tags.map((tag) => (
+                    <option key={tag.id} value={tag.id}>{tag.name}</option>
+                ))}
+            </select>
 
             <label htmlFor="body">Body:</label>
             <textarea
