76ed092400df6f9d35754d4a06ca6559ecf28713
Azran Hossain <mr69751@gmail.com>
2023-04-28 20:06:39 +0600

    user can now add comment

--- app/journal/[id]/page.tsx
+++ app/journal/[id]/page.tsx
@@ -1,11 +1,13 @@
 'use client';
-import { useEffect, useState } from "react";
+import { Comment, JournalEntry, Props } from "@/app/Types";
 import axiosWithAuth from "@/app/lib/AxiosAuth";
-import { JournalEntry, Props } from "@/app/Types";
+import { useEffect, useState } from "react";
 
 const JournalEntryView = ({ params }: Props) => {
     const id = params.id;
     const [journalEntry, setJournalEntry] = useState<JournalEntry | null>(null);
+    const [comments, setComments] = useState<Comment[]>([]);
+    const [userId, setUserId] = useState<number>();
 
     useEffect(() => {
         axiosWithAuth
@@ -16,7 +18,43 @@ const JournalEntryView = ({ params }: Props) => {
             .catch((error) => {
                 console.error(error);
             });
-    }, [id]);
+        axiosWithAuth.get(`/user/${sessionStorage.getItem("username")}`).then((response) => {
+            setUserId(response.data.id);
+        });
+    }, []);
+
+    useEffect(() => {
+        axiosWithAuth
+            .get(`/journal-comments/${id}`)
+            .then((response) => {
+                setComments(...comments, response.data.reverse());
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, []);
+
+    const handleCommentSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const formData = new FormData(event.currentTarget);
+        const newComment = {
+            body: formData.get("body") as string,
+            journal_entry_id: id,
+            user_id: userId,
+        };
+        axiosWithAuth
+            .post("/journal-comments", newComment)
+            .then((response) => {
+                setComments([...comments, response.data]);
+                //reset form
+                event.currentTarget.reset();
+                window.location.reload();
+            })
+            .catch((error) => {
+                window.location.reload();
+                console.error(error);
+            });
+    };
 
     if (!journalEntry) {
         return <div>Loading...</div>;
@@ -27,9 +65,38 @@ const JournalEntryView = ({ params }: Props) => {
             <h1 className="px-3 text-xl font-bold">{journalEntry.title}</h1>
             <p className="px-3 mt-3 text-sm text-gray-500">
                 Written by {journalEntry.user.name} on{" "}
-                {new Date(...journalEntry.updatedAt).toLocaleDateString()}
+                {new Date(...journalEntry.createdAt).toLocaleDateString()}
             </p>
             <p className="p-3 my-2 border border-gray-200">{journalEntry.body}</p>
+            <form className="flex flex-col w-1/2 mx-auto my-5" onSubmit={handleCommentSubmit}>
+                <label className="text-gray-700">Add a comment:</label>
+                <textarea name="body" className="w-full h-24 p-2 mt-2 text-gray-700 border border-gray-400 resize-none" required></textarea>
+                <button type="submit" className="px-4 py-2 mt-4 text-white bg-blue-500 rounded hover:bg-blue-600 focus:bg-blue-700 focus:outline-none">
+                    Submit
+                </button>
+            </form>
+            <h2 className="px-3 mt-5 mb-3 text-lg font-bold">Comments</h2>
+            {comments.length > 0 ? (
+                <ul className="divide-y divide-gray-200">
+                    {comments.map((comment) => (
+                        <li key={comment.id} className="py-4">
+                            <div className="flex space-x-3">
+                                <div className="flex-shrink-0">
+                                    <img className="w-10 h-10 rounded-full" src={comment.userImage} alt="" />
+                                </div>
+                                <div className="flex-1 min-w-0">
+                                    <p className="text-sm font-medium text-gray-900">{comment.username}</p>
+                                    {comment.created_at && <p className="text-sm text-gray-500">{new Date(...comment.created_at).toLocaleDateString()}</p>}
+                                    <p className="mt-1 text-sm text-gray-700">{comment.body}</p>
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No comments yet.</p>
+            )}
+
         </div>
     );
 };
